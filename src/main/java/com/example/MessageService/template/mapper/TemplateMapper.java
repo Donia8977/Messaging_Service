7005f3007eb516575ef8e35f1afc2a6973e1d946
DoniaAhmed <doniaelsharony@gmail.com>
2025-07-07 10:36:09 +0300

    WIP: save local changes before pulling auth that contains message

--- src/main/java/com/example/MessageService/template/mapper/TemplateMapper.java
+++ src/main/java/com/example/MessageService/template/mapper/TemplateMapper.java
@@ -1,5 +1,24 @@
+//package com.example.MessageService.template.mapper;
+//
+//import org.mapstruct.Mapper;
+//import org.mapstruct.Mapping;
+//import org.mapstruct.factory.Mappers;
+//import com.example.MessageService.template.dto.TemplateResponse;
+//import com.example.MessageService.template.entity.Template;
+//
+//
+//import javax.xml.transform.Source;
+//
+//@Mapper(componentModel = "spring")
+//public interface TemplateMapper {
+//
+//
+//    TemplateResponse mapToResponse(Template template);
+//}
+
 package com.example.MessageService.template.mapper;
 
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -8,14 +27,19 @@ import com.example.MessageService.template.entity.Template;
 
 
 import javax.xml.transform.Source;
+import java.beans.Expression;
 
 @Mapper(componentModel = "spring")
 public interface TemplateMapper {
 
+<<<<<<< Updated upstream
 //    TemplateMapper INSTANCE = Mappers.getMapper(TemplateMapper.class);
 //    @Mapping(source = "id" ,target = "id")
 //    @Mapping(source = "name" ,target = "name")
 //    @Mapping(source = "content" ,target = "content")
 //    @Mapping(source = "createdAt" ,target = "createdAt")
+=======
+
+>>>>>>> Stashed changes
     TemplateResponse mapToResponse(Template template);
 }
