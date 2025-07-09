01fb53d107d666c1736ed26078bd5a18839fadaa
DoniaAhmed <doniaelsharony@gmail.com>
2025-07-09 23:29:17 +0300

    implementing the architecture of Kafka

---
+++ src/main/java/com/example/MessageService/message/Kafka/config/KafkaConsumerConfig.java
@@ -0,0 +1,4 @@
+package com.example.MessageService.message.Kafka.config;
+
+public class KafkaConsumerConfig {
+}
---
+++ src/main/java/com/example/MessageService/message/Kafka/provider/impl/SendGridEmailProvider.java
@@ -0,0 +1,4 @@
+package com.example.MessageService.message.Kafka.provider.impl;
+
+public class SendGridEmailProvider {
+}
