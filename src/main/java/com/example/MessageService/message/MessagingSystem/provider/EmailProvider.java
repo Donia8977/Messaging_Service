2800bee207346d9ea91da84368a31e88851c2bfd
ahmed <ahmedmohamedabdelmgead@gmail.com>
2025-07-11 08:11:46 +0300

    Update Email Provider

--- src/main/java/com/example/MessageService/message/MessagingSystem/provider/EmailProvider.java
+++ src/main/java/com/example/MessageService/message/MessagingSystem/provider/EmailProvider.java
@@ -2,7 +2,7 @@ package com.example.MessageService.message.MessagingSystem.provider;
 
 import com.example.MessageService.message.entity.Message;
 
-public interface Provider {
+public interface EmailProvider {
 
  void send(Message message);
 
--- src/main/java/com/example/MessageService/message/MessagingSystem/provider/EmailProviderImpl.java
+++ src/main/java/com/example/MessageService/message/MessagingSystem/provider/EmailProviderImpl.java
@@ -1,31 +1,22 @@
 package com.example.MessageService.message.MessagingSystem.provider;
 
-import com.example.MessageService.message.entity.EmailDetails;
 import com.example.MessageService.message.entity.Message;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
-import org.springframework.mail.MailAuthenticationException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-//@Profile("Kafka")
 @Slf4j
-public class EmailProviderImpl implements Provider {
+@AllArgsConstructor
+public class EmailProviderImpl implements EmailProvider {
 
     private final JavaMailSender mailSender;
-
-
-    public EmailProviderImpl(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
-
 
     @Override
     public void send(Message message) {
         try {
-
             String from = message.getTenant().getEmail();
             String to = message.getUser().getEmail();
             String subject = "Your message from " + message.getTenant().getName();
@@ -41,22 +32,6 @@ public class EmailProviderImpl implements Provider {
             log.info("Email sent from {} to {}", from, to);
         } catch (Exception e) {
             log.error("Error sending email: {}", e.getMessage(), e);
-        }
-    }
-
-    public void sendDirectEmail(EmailDetails details) {
-        try {
-
-            SimpleMailMessage email = new SimpleMailMessage();
-            email.setTo(details.getTo());
-            email.setSubject(details.getSubject());
-            email.setText(details.getContent());
-
-            mailSender.send(email);
-            log.info("Direct email sent successfully to {}", details.getTo());
-        } catch (Exception e) {
-            log.error("Error sending direct email to {}: {}", details.getTo(), e.getMessage(), e);
-            throw new MailAuthenticationException("Failed to send direct email", e);
         }
     }
 
