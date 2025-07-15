a8b2d870929ff4e66e98baae0a7dfbda20fd1775
ahmed <ahmedmohamedabdelmgead@gmail.com>
2025-07-15 11:03:16 +0300

    Server port Update

--- src/main/java/com/example/MessageService/MessageServiceApplication.java
+++ src/main/java/com/example/MessageService/MessageServiceApplication.java
@@ -1,8 +1,11 @@
 package com.example.MessageService;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class MessageServiceApplication {
@@ -10,9 +13,6 @@ public class MessageServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MessageServiceApplication.class, args);
 
-//		String rawPassword = "Admin@123.";
-//		String hashed = new BCryptPasswordEncoder().encode(rawPassword);
-//		System.out.println("BCrypt hash for Admin password: " + hashed);
 	}
 	
 }
